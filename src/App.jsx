62accfb0a3cc7deddd6d0c915b24be6c7a87c4be
Meenakshi <83496336+Memeena@users.noreply.github.com>
2024-05-16 17:00:26 +0530

    personal details page design complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import PersonalDetails from "./Components/MainPages/PersonalDetails/PersonalDetails";
 import SideBar from "./Components/SideBar/SideBar";
 import "./index.css";
 
 function App() {
   return (
     <div className="app">
-      <SideBar />
-      <h1>Details</h1>
+      <SideBar className="sideBar" />
+      <div className="mainPage">
+        <PersonalDetails />
+      </div>
+      <div className="button">
+        <button className="btn-back">Go back</button>
+        <button className="btn-next">Next Step</button>
+      </div>
     </div>
   );
 }
--- src/Components/MainPages/PersonalDetails/PersonalDetails.jsx
+++ src/Components/MainPages/PersonalDetails/PersonalDetails.jsx
@@ -1,5 +1,56 @@
-import styles from "./PersonaDetails.module.css";
+import styles from "./PersonalDetails.module.css";
+import "../../../index.css";
 
-export default function PersonaDetails() {
-  return <h1>PersonaDetails</h1>;
+export default function PersonalDetails() {
+  return (
+    <div className={styles.personalDetails}>
+      <h1 className={`${styles.heading} ${"headingMedium"}`}>Personal info</h1>
+      <p className={`${styles.subHeading} ${"p-large"}`}>
+        Please provide your name, email address, and phone number.
+      </p>
+
+      <div className={styles.details}>
+        <div className={`${styles.det} ${"p-medium"}`}>
+          <label htmlFor="name" className={styles.detHeading}>
+            Name
+          </label>
+          <input
+            id="name"
+            className={styles.input}
+            type="text"
+            placeholder="e.g. Stephen King"
+            // onChange={handleEnteredWord}
+            // value={enteredWord}
+            // style={inputStyle}
+          ></input>
+        </div>
+
+        <div className={`${styles.det} ${"p-medium"}`}>
+          <label htmlFor="email" className={styles.detHeading}>
+            Email address
+          </label>
+
+          <input
+            id="email"
+            className={styles.input}
+            type="email"
+            placeholder="e.g. stephenking@lorem.com"
+          ></input>
+        </div>
+
+        <div className={`${styles.det} ${"p-medium"}`}>
+          <label htmlFor="phoneNo" className={styles.detHeading}>
+            Phone Number
+          </label>
+
+          <input
+            id="phoneNo"
+            className={styles.input}
+            type="text"
+            placeholder="e.g. +1 234 567 890"
+          ></input>
+        </div>
+      </div>
+    </div>
+  );
 }
