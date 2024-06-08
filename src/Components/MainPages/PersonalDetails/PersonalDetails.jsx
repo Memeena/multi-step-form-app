ccd2a4f50abae9530b02ea239b381a1deef6f42b
Meenakshi <83496336+Memeena@users.noreply.github.com>
2024-06-08 16:26:50 +0530

    changes in personalDetails file

--- src/Components/MainPages/PersonalDetails/PersonalDetails.jsx
+++ src/Components/MainPages/PersonalDetails/PersonalDetails.jsx
@@ -1,25 +1,39 @@
 import styles from "./PersonalDetails.module.css";
 import "../../../index.css";
 import Header from "../../Header/Header";
+import { useState } from "react";
 
 export default function PersonalDetails() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [contact, setContact] = useState(0);
+
+  function handleChange(e) {
+    console.log(e.target.value);
+    setName(e.target.value);
+  }
   return (
     <div className={styles.personalDetails}>
-      <Header heading={'Personal Info'} subHeading={'Please provide your name, email address, and phone number.'}/>     
+      <Header
+        heading={"Personal Info"}
+        subHeading={
+          "Please provide your name, email address, and phone number."
+        }
+      />
 
       <div className={styles.details}>
         <div className={`${styles.det} ${"p-medium"}`}>
           <label htmlFor="name" className={styles.detHeading}>
             Name
           </label>
+          {!name && <p>This field is required!</p>}
           <input
             id="name"
             className={styles.input}
             type="text"
             placeholder="e.g. Stephen King"
-            // onChange={handleEnteredWord}
-            // value={enteredWord}
-            // style={inputStyle}
+            value={name}
+            onChange={(e) => handleChange(e)}
           ></input>
         </div>
 
@@ -33,6 +47,7 @@ export default function PersonalDetails() {
             className={styles.input}
             type="email"
             placeholder="e.g. stephenking@lorem.com"
+            value={email}
           ></input>
         </div>
 
@@ -46,6 +61,7 @@ export default function PersonalDetails() {
             className={styles.input}
             type="text"
             placeholder="e.g. +1 234 567 890"
+            value={contact}
           ></input>
         </div>
       </div>
